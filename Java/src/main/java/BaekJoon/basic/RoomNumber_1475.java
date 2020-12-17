6ae2b3a8ec3b340f10ea4460837a7f4e0b1fbd17
201411205 <bibiq5045@gmail.com>
2020-12-17 15:06:24 +0900

    - 실버 5 1475번 방 번호

---
+++ Java/src/main/java/BaekJoon/basic/RoomNumber_1475.java
@@ -0,0 +1,31 @@
+package BaekJoon.basic;
+
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static Util.Constants.INPUT;
+
+public class RoomNumber_1475 {
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream(INPUT));
+        Scanner sc = new Scanner(System.in);
+        final String str = sc.next();
+        int result = 0;
+        int[] arr = new int[10];
+        Arrays.fill(arr, 0);
+        for(int i = 0; i < str.length(); i++) {
+            int n = str.charAt(i) - '0';
+            arr[n]++;
+        }
+        int max = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(i != 6 && i != 9) {
+                max = Math.max(max, arr[i]);
+            }
+        }
+        result = Math.max(max, (arr[6] + arr[9] + 1) / 2);
+        System.out.println(result);
+        sc.close();
+    }
+}
