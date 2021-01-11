2dc2653b11d62d1a88e77c88bb42aa4603e5a6d6
Gun <bibiq5045@gmail.com>
2021-01-12 07:17:53 +0900

    스택 - 4889 안정적인 문자열 - 2504 괄호의 값 BFS - 1926 그림 - 4179 불!

---
+++ Java/src/main/java/BaekJoon/BFS/Drawing_1926.java
@@ -0,0 +1,71 @@
+package BaekJoon.BFS;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+import static Util.Constants.INPUT;
+
+public class Drawing_1926 {
+    static int MAX = 501;
+    static int[] ax = {-1,1,0,0};
+    static int[] ay = {0,0,-1,1};
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream(INPUT));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        final int n = Integer.parseInt(st.nextToken());
+        final int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[MAX][MAX];
+        boolean[][] check = new boolean[MAX][MAX];
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                check[i][j] = false;
+            }
+        }
+        int count = 0, max = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(arr[i][j] == 1 && !check[i][j]) {
+                    count++;
+                    Queue<Pair> q = new LinkedList<>();
+                    int area = 1;
+                    check[i][j] = true;
+                    q.add(new Pair(i, j));
+                    while(!q.isEmpty()) {
+                        int cx = q.peek().x;
+                        int cy = q.peek().y;
+                        q.poll();
+                        for(int k = 0; k < 4; k++) {
+                            int nx = cx + ax[k];
+                            int ny = cy + ay[k];
+                            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                            if(arr[nx][ny] == 1 && !check[nx][ny]) {
+                                check[nx][ny] = true;
+                                area++;
+                                q.add(new Pair(nx, ny));
+                            }
+                        }
+                    }
+                    max = Math.max(max, area);
+                }
+            }
+        }
+        System.out.println(count);
+        System.out.println(max);
+        br.close();
+    }
+}
+
+class Pair {
+    int x, y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ Java/src/main/java/BaekJoon/BFS/Fire_4179.java
@@ -0,0 +1,86 @@
+package BaekJoon.BFS;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+import static Util.Constants.INPUT;
+
+public class Fire_4179 {
+    static int MAX = 1001;
+    static int[] ax = {-1,1,0,0};
+    static int[] ay = {0,0,-1,1};
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream(INPUT));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        final int R = Integer.parseInt(st.nextToken());
+        final int C = Integer.parseInt(st.nextToken());
+        String[] strings = new String[MAX];
+        int[][] arr1 = new int [MAX][MAX];
+        int[][] arr2 = new int [MAX][MAX];
+        Queue<pair> fq = new LinkedList<>();
+        Queue<pair> jq = new LinkedList<>();
+        for(int i = 0; i < R; i++) {
+            String str = br.readLine();
+            strings[i] = str;
+            for(int j = 0; j < C; j++) {
+                char c = str.charAt(j);
+                arr1[i][j] = -1;
+                arr2[i][j] = -1;
+                if(c == 'F') {
+                    fq.add(new pair(i, j));
+                    arr1[i][j] = 0;
+                }
+                if(c == 'J') {
+                    jq.add(new pair(i, j));
+                    arr2[i][j] = 0;
+                }
+            }
+        }
+        while(!fq.isEmpty()) {
+            int cx = fq.peek().x;
+            int cy = fq.peek().y;
+            int num = arr1[cx][cy];
+            fq.poll();
+            for(int i = 0; i < 4; i++) {
+                int nx = cx + ax[i];
+                int ny = cy + ay[i];
+                if(nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+                if(arr1[nx][ny] >= 0 || strings[nx].charAt(ny) == '#') continue;
+                arr1[nx][ny] = num + 1;
+                fq.add(new pair(nx, ny));
+            }
+        }
+        while(!jq.isEmpty()) {
+            int cx = jq.peek().x;
+            int cy = jq.peek().y;
+            int num = arr2[cx][cy];
+            jq.poll();
+            for(int i = 0; i < 4; i++) {
+                int nx = cx + ax[i];
+                int ny = cy + ay[i];
+                if(nx < 0 || nx >= R || ny < 0 || ny >= C) {
+                    System.out.println(num + 1);
+                    return;
+                }
+                if(arr2[nx][ny] >= 0 || strings[nx].charAt(ny) == '#') continue;
+                if(arr1[nx][ny] != -1 && arr1[nx][ny] <= num + 1) continue;
+                arr2[nx][ny] = num + 1;
+                jq.add(new pair(nx, ny));
+            }
+        }
+        System.out.println("IMPOSSIBLE");
+    }
+
+    static class pair {
+        int x, y;
+        pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
