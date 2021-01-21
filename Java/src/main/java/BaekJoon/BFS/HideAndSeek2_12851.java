3d5d6e4ddbc2a819efd8dac8e3cbd0276305f7c6
201411205 <bibiq5045@gmail.com>
2021-01-21 18:01:23 +0900

    BFS - 1697 숨바꼭질1 - 13549 숨바꼭질 3 - 아직 못품

---
+++ Java/src/main/java/BaekJoon/BFS/HideAndSeek2_12851.java
@@ -0,0 +1,18 @@
+package BaekJoon.BFS;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+import static Util.Constants.INPUT;
+
+public class HideAndSeek2_12851 {
+    static int MAX = 100001;
+    static boolean[] check;
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream(INPUT));
+        Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
+        final int K = sc.nextInt();
+
+    }
+}
