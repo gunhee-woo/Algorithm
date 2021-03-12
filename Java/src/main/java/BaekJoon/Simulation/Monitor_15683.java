660b419bf649f179391fa3b8c921883924e9c857
201411205 <bibiq5045@gmail.com>
2021-03-12 17:59:51 +0900

    시뮬레이션(백트래킹) - 감시 15683 - 스티커 붙이기 18808

--- Java/src/main/java/BaekJoon/Simulation/Monitor_15683.java
+++ Java/src/main/java/BaekJoon/Simulation/Monitor_15683.java
@@ -10,13 +10,14 @@ import java.util.StringTokenizer;
 
 import static Util.Constants.INPUT;
 
+// 시간초과 코드
 public class Monitor_15683 {
     static int MAX = 9;
     static int N, M;
     static int[][] map;
-    static List<pair> list;
-    static int count = 0; // map에 존재하는 0의 개수
-    static int result = 987654321;
+    static int[][] cmap;
+    static List<cctv> list;
+    static int count = 987654321;
     public static void main(String[] args) throws Exception{
         System.setIn(new FileInputStream(INPUT));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,119 +30,162 @@ public class Monitor_15683 {
             int[] a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             for(int j = 0; j < M; j++) {
                 map[i][j] = a[j];
-                if(map[i][j] != 0 && map[i][j] != 6) list.add(new pair(i, j));
+                if(map[i][j] != 0 && map[i][j] != 6) list.add(new cctv(i, j, map[i][j], -1));
                 if(map[i][j] == 0) count++;
             }
         }
-        dfs(0, 0);
-        System.out.print(result);
+        dfs(0);
+        System.out.print(count);
     }
 
-    static void dfs(int k, int cnt) {
+    static void dfs(int k) {
         if(k == list.size()) {
-            result = Math.min(result, count - cnt);
+            search();
+            int cnt = 0;
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    if(cmap[i][j] == 0) cnt++;
+                }
+            }
+            count = Math.min(count, cnt);
             return;
         }
+        list.get(k).dir = 0; // up
+        dfs(k + 1);
 
-        int[][] map2 = map;
+        list.get(k).dir = 1; // down
+        dfs(k + 1);
 
-        for(int i = k; i < list.size(); i++) {
-            int ax = list.get(i).x;
-            int ay = list.get(i).y;
-            if(map[ax][ay] == 1) {
-                for(int j = 0; j < 4; j++) {
-                    if(j == 0) dfs(k + 1, cnt + left(ax, ay));
-                    if(j == 1) dfs(k + 1, cnt + right(ax, ay));
-                    if(j == 2) dfs(k + 1, cnt + up(ax, ay));
-                    if(j == 3) dfs(k + 1, cnt + down(ax, ay));
-                }
-            } else if(map[ax][ay] == 2) {
-                for(int j = 0; j < 2; j++) {
-                    if(j == 0) dfs(k + 1, cnt + left(ax, ay) + right(ax, ay));
-                    if(j == 1) dfs(k + 1, cnt + up(ax, ay) + down(ax, ay));
-                }
-            } else if(map[ax][ay] == 3) {
-                for(int j = 0; j < 4; j++) {
-                    if(j == 0) dfs(k + 1, cnt + up(ax, ay) + right(ax, ay));
-                    if(j == 1) dfs(k + 1, cnt + right(ax, ay) + down(ax, ay));
-                    if(j == 2) dfs(k + 1, cnt + left(ax, ay) + down(ax, ay));
-                    if(j == 3) dfs(k + 1, cnt + up(ax, ay) + left(ax, ay));
-                }
-            } else if(map[ax][ay] == 4) {
-                for(int j = 0; j < 4; j++) {
-                    if(j == 0) dfs(k + 1, cnt + up(ax, ay) + right(ax, ay) + left(ax, ay));
-                    if(j == 1) dfs(k + 1, cnt + up(ax, ay) + right(ax, ay) + down(ax, ay));
-                    if(j == 2) dfs(k + 1, cnt + right(ax, ay) + left(ax, ay) + down(ax, ay));
-                    if(j == 3) dfs(k + 1, cnt + down(ax, ay) + up(ax, ay) + left(ax, ay));
-                }
-            } else if(map[ax][ay] == 5) {
-                dfs(k + 1, cnt + up(ax, ay) + right(ax, ay) + left(ax, ay) + down(ax, ay));
+        list.get(k).dir = 2; // left
+        dfs(k + 1);
+
+        list.get(k).dir = 3; // right
+        dfs(k + 1);
+    }
+
+    static void search() {
+        cmap = map;
+        for(int i = 0; i < list.size(); i++) {
+            cctv ctv = list.get(i);
+            if(ctv.num == 1) {
+                if(ctv.dir == 0) up(ctv.x, ctv.y);
+                else if(ctv.dir == 1) down(ctv.x, ctv.y);
+                else if(ctv.dir == 2) left(ctv.x, ctv.y);
+                else if(ctv.dir == 3) right(ctv.x, ctv.y);
+            } else if(ctv.num == 2) {
+                if(ctv.dir == 0 || ctv.dir == 1) { up(ctv.x, ctv.y); down(ctv.x, ctv.y);}
+                else if(ctv.dir == 2 || ctv.dir == 3) { left(ctv.x, ctv.y); right(ctv.x, ctv.y);}
+            } else if(ctv.num == 3) {
+                if(ctv.dir == 0) { up(ctv.x, ctv.y); right(ctv.x, ctv.y);}
+                else if(ctv.dir == 1) { right(ctv.x, ctv.y); down(ctv.x, ctv.y);}
+                else if(ctv.dir == 2) { down(ctv.x, ctv.y); left(ctv.x, ctv.y);}
+                else if(ctv.dir == 3) { left(ctv.x, ctv.y); up(ctv.x, ctv.y);}
+            } else if(ctv.num == 4) {
+                if(ctv.dir == 0) { up(ctv.x, ctv.y); right(ctv.x, ctv.y); left(ctv.x, ctv.y);}
+                else if(ctv.dir == 1) { right(ctv.x, ctv.y); down(ctv.x, ctv.y); up(ctv.x, ctv.y);}
+                else if(ctv.dir == 2) { down(ctv.x, ctv.y); left(ctv.x, ctv.y); right(ctv.x, ctv.y);}
+                else if(ctv.dir == 3) { left(ctv.x, ctv.y); up(ctv.x, ctv.y); down(ctv.x, ctv.y); }
+            } else if(ctv.num == 5) {
+                left(ctv.x, ctv.y); right(ctv.x, ctv.y); up(ctv.x, ctv.y); down(ctv.x, ctv.y);
             }
         }
     }
 
-    static void mapCopy() {
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
+//    static void dfs(int k) {
+//        if(k == list.size()) {
+//            int cnt = 0;
+//            for(int i = 0; i < N; i++) {
+//                for(int j = 0; j < M; j++) {
+//                    if(cmap[i][j] == 0) cnt++;
+//                }
+//            }
+//            count = Math.min(count, cnt);
+//            return;
+//        }
+//        int ax = list.get(k).x;
+//        int ay = list.get(k).y;
+//        if(map[ax][ay] == 1) {
+//            for(int i = 0; i < 4; i++) {
+//                if(i == 0) up(ax, ay);
+//                if(i == 1) down(ax, ay);
+//                if(i == 2) left(ax, ay);
+//                if(i == 3) right(ax, ay);
+//                dfs(k + 1);
+//            }
+//        } else if(map[ax][ay] == 2) {
+//            for(int i = 0; i < 2; i++) {
+//                if(i == 0) { left(ax, ay); right(ax, ay);}
+//                if(i == 1) { up(ax, ay); down(ax, ay); }
+//                dfs(k + 1);
+//            }
+//        } else if(map[ax][ay] == 3) {
+//            for(int i = 0; i < 4; i++) {
+//                if(i == 0) { up(ax, ay); right(ax, ay); }
+//                if(i == 1) { right(ax, ay); down(ax, ay); }
+//                if(i == 2) { left(ax, ay); down(ax, ay); }
+//                if(i == 3) { up(ax, ay); left(ax, ay); }
+//                dfs(k + 1);
+//            }
+//        } else if(map[ax][ay] == 4) {
+//            for(int i = 0; i < 4; i++) {
+//                if(i == 0) { left(ax, ay); up(ax, ay); right(ax, ay); }
+//                if(i == 1) { up(ax, ay); right(ax, ay); down(ax, ay); }
+//                if(i == 2) { right(ax, ay); left(ax, ay); down(ax, ay); }
+//                if(i == 3) { up(ax, ay); left(ax, ay); down(ax, ay); }
+//                dfs(k + 1);
+//            }
+//        } else if(map[ax][ay] == 5) {
+//            left(ax, ay); up(ax, ay); right(ax, ay); down(ax, ay);
+//            dfs(k + 1);
+//        }
+//    }
 
-            }
-        }
-    }
-
-    static int left(int x, int y) {
-        int cnt = 0;
+    static void up(int x, int y) {
         for(int i = x - 1; i >= 0; i--) {
-            if(map[i][y] == 6) break;
-            if(map[i][y] == 0) {
-                map[i][y] = 7;
-                cnt++;
+            if(cmap[i][y] == 6) break;
+            if(cmap[i][y] == 0) {
+                cmap[i][y] = 7;
             }
         }
-        return cnt;
     }
 
-    static int right(int x, int y) {
-        int cnt = 0;
-        for(int i = x + 1; i < M; i++) {
-            if(map[i][y] == 6) break;
-            if(map[i][y] == 0) {
-                map[i][y] = 7;
-                cnt++;
+    static void down(int x, int y) {
+        for(int i = x + 1; i < N; i++) {
+            if(cmap[i][y] == 6) break;
+            if(cmap[i][y] == 0) {
+                cmap[i][y] = 7;
             }
         }
-        return cnt;
     }
 
-    static int up(int x, int y) {
-        int cnt = 0;
+    static void left(int x, int y) {
         for(int i = y - 1; i >= 0; i--) {
-            if(map[x][i] == 6) break;
-            if(map[x][i] == 0) {
-                map[x][i] = 7;
-                cnt++;
+            if(cmap[x][i] == 6) break;
+            if(cmap[x][i] == 0) {
+                cmap[x][i] = 7;
             }
         }
-        return cnt;
     }
 
-    static int down(int x, int y) {
-        int cnt = 0;
-        for(int i = y + 1; i < N; i++) {
-            if (map[x][i] == 6) break;
-            if(map[x][i] == 0) {
-                map[x][i] = 7;
-                cnt++;
+    static void right(int x, int y) {
+        for(int i = y + 1; i < M; i++) {
+            if(cmap[x][i] == 6) break;
+            if(cmap[x][i] == 0) {
+                cmap[x][i] = 7;
             }
         }
-        return cnt;
     }
 
-    static class pair {
+    static class cctv {
         int x;
         int y;
-        pair(int x, int y) {
+        int num;
+        int dir;
+        cctv(int x, int y, int num, int dir) {
             this.x = x;
             this.y = y;
+            this.num = num;
+            this.dir = dir;
         }
     }
 }
