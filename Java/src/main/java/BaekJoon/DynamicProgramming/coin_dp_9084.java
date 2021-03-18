536cf1658ebd5f192667646b04b984badf51278f
201411205 <bibiq5045@gmail.com>
2021-03-18 17:58:38 +0900

    다이나믹 프로그래밍 - 알약 4811 - 코인 9084 진행중

---
+++ Java/src/main/java/BaekJoon/DynamicProgramming/coin_dp_9084.java
@@ -0,0 +1,19 @@
+package BaekJoon.DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import static Util.Constants.INPUT;
+
+public class coin_dp_9084 {
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream(INPUT));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while(T -- > 0) {
+            int N = Integer.parseInt(br.readLine());
+
+        }
+    }
+}
