6f7c89f512e14e857d7d9d04247d2127131d5653
201411205 <bibiq5045@gmail.com>
2021-04-19 18:03:59 +0900

    이진탐색 - 수들의 합 1789 - 정수제곱식

---
+++ Java/src/main/java/BaekJoon/BinarySearch/IntegerSquareRoot_2417.java
@@ -0,0 +1,24 @@
+package BaekJoon.BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import static Util.Constants.INPUT;
+
+public class IntegerSquareRoot_2417 {
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream(INPUT));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        long start = 0, end = (long) Math.sqrt(N);
+        long mid = 0;
+        while(start <= end) {
+            mid = (start + end) / 2;
+            if(mid >= Math.sqrt(N)) end = mid - 1;
+            else start = mid + 1;
+        }
+        if(mid * mid == N) System.out.print(mid);
+        else System.out.print(mid + 1);
+    }
+}
