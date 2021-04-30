631aa7de14ff1ad8196d149eda040e05071e580e
201411205 <bibiq5045@gmail.com>
2021-04-30 18:00:25 +0900

    브루트 포스 - 청소년 상어 19236 진행중

--- Java/src/main/java/BaekJoon/Simulation/SamsungProblem_A/JuvenileShark_19236.java
+++ Java/src/main/java/BaekJoon/Simulation/SamsungProblem_A/JuvenileShark_19236.java
@@ -3,6 +3,7 @@ package BaekJoon.Simulation.SamsungProblem_A;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
@@ -18,6 +19,7 @@ public class JuvenileShark_19236 {
         System.setIn(new FileInputStream(INPUT));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         map = new int[4][4];
+        mp  = new HashMap<>();
         for(int i = 0; i < 4; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0; j < 4; j++) {
@@ -28,28 +30,50 @@ public class JuvenileShark_19236 {
             }
         }
         sk = new Fish(0, 0, mp.get(map[0][0]).dir);
-        print();
+        mp.put(map[0][0], new Fish(0, 0, 0));
+        map[0][0] = 0;
+        while(true) {
+            moveFish();
+            int nx = sk.x + ax[sk.dir];
+            int ny = sk.y + ay[sk.dir];
+            if(nx < 0 || nx >= 4 || ny < 0 || ny >= 4 || map[nx][ny] == 0) continue;
+
+
+        }
+//        print();
+    }
+
+    private static void dfs(int sum) {
+        if() {
+
+            return;
+        }
+        dfs();
     }
 
     private static void moveFish() {
         for(int i = 1; i < 16; i++) {
             Fish cf = mp.get(i);
+            if(map[cf.x][cf.y] == 0) continue;
             int nx = cf.x + ax[cf.dir];
             int ny = cf.y + ay[cf.dir];
             int nd = cf.dir;
             while(nx < 0 || nx >= 4 || ny < 0 || ny >= 4 || (nx == sk.x && ny == sk.y)) { // 이동할 수 없으면 45도 각도로 회전
                 nd++;
-                if(nd == 9) nd = 0;
+                if(nd == 9) nd = 1;
                 if(nd == cf.dir) { // 이동 못함
                     return;
                 }
-                nx += ax[nd];
-                ny += ay[nd];
+                nx = cf.x + ax[nd];
+                ny = cf.y + ay[nd];
             }
-            Fish temp = mp.get(map[nx][ny]);
-            mp.put(map[cf.x][cf.y], new Fish(temp.x, temp.y, temp.dir));
-            mp.put(map[nx][ny], new Fish(nx, ny, nd));
-            cf = new Fish(nx, ny, mp.get(map[nx][ny]).dir);
+            mp.put(map[cf.x][cf.y], new Fish(nx, ny, cf.dir)); // Map 안에 있는 현재 번호의 x, y값 업데이트
+            mp.put(map[nx][ny], new Fish(cf.x, cf.y, mp.get(map[nx][ny]).dir));
+            int temp = map[cf.x][cf.y]; // 지도상의 번호 위치 스위칭
+            map[cf.x][cf.y] = map[nx][ny];
+            map[nx][ny] = temp;
+            print();
+            System.out.println(i + "---");
         }
     }
 
@@ -58,7 +82,7 @@ public class JuvenileShark_19236 {
             for(int j = 0; j < 4; j++) {
                 System.out.print(map[i][j] + " ");
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 
