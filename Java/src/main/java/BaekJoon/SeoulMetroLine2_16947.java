f37f714c3d0d1b35e4c55b5dfb80ba4080d7e0aa
201411205 <bibiq5045@gmail.com>
2021-06-25 17:33:38 +0900

    시뮬레이션 - 16947 서울 지하철 2호선

--- Java/src/main/java/BaekJoon/SeoulMetroLine2_16947.java
+++ Java/src/main/java/BaekJoon/SeoulMetroLine2_16947.java
@@ -3,10 +3,7 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 import static Util.Constants.INPUT;
 
@@ -35,6 +32,13 @@ public class SeoulMetroLine2_16947 {
             Arrays.fill(check, false);
             dfs(i, i, 0);
         }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= N; i++) {
+            Arrays.fill(check, false);
+            if(!cycle[i]) sb.append(bfs(new Pair(i, 1))).append(" ");
+            else sb.append("0 ");
+        }
+        System.out.print(sb.toString());
     }
 
     private static void dfs(int cur, int start, int cnt) {
@@ -48,8 +52,43 @@ public class SeoulMetroLine2_16947 {
             if(!check[next]) {
                 dfs(next, start, cnt + 1);
             } else {
-                if(next == )
+                if(next == start && cnt >= 2) {
+                    dfs(next, start, cnt + 1);
+                }
             }
+        }
+    }
+
+    private static String bfs(Pair node) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(node);
+        int answer = 0;
+        boolean isFind = false;
+        while(!q.isEmpty()) {
+            Pair cur = q.poll();
+            for(int i = 0; i < list[cur.node].size(); i++) {
+                int next = list[cur.node].get(i);
+                if(!check[next]) {
+                    check[next] = true;
+                    if(cycle[next]) {
+                        isFind = true;
+                        answer = cur.cnt;
+                        break;
+                    } else {
+                        q.add(new Pair(next, cur.cnt + 1));
+                    }
+                }
+            }
+            if(isFind) break;
+        }
+        return String.valueOf(answer);
+    }
+
+    private static class Pair {
+        int node; int cnt;
+        Pair(int node, int cnt) {
+            this.node = node;
+            this.cnt = cnt;
         }
     }
 }
