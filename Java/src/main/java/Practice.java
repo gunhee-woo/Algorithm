3949ad0d7740fa4e1c1d25b8f45aeb3ef764b874
201411205 <bibiq5045@gmail.com>
2021-12-08 10:05:53 +0900

    test

---
+++ Java/src/main/java/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+    public static void main(String[] args) {
+        print("Practice");
+    }
+
+    public static void print(Object o) {
+        System.out.print(o);
+    }
+}
